5ef98df87c1515410ceba7881a2d6ec783e5ac48
Kajal Chakole <kajal.chakole@getvymo.com>
2024-06-18 10:55:26 +0530

    Added Meal Item and tile clicks

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,13 +2,19 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-}
-function CategoriesScreen() {
+function CategoriesScreen({ navigation }) {
+
+    function renderCategoryItem(itemData) {
+
+        function pressHandler(id) {
+            navigation.navigate('MealsOverview', { categoryId: itemData.item.id });
+        }
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />
+    }
+
     return (
-       <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem}
-       numColumns={2}/>
+        <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem}
+            numColumns={2} />
     );
 }
 
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+    const categoryId = route.params.categoryId; 
+    const displayedMeals = MEALS.filter((meal) => {
+        return meal.categoryIds.indexOf(categoryId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
+        </View>
+    );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    }
+});
