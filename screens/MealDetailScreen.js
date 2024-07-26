3e16fc8ae310c3d9e27e34185f0da743eeb7c5ec
Kajal Chakole <kajal.chakole@getvymo.com>
2024-07-26 19:49:18 +0530

    Added Header Button in Navigator

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,27 @@
-import { Text, Image, View, StyleSheet, ScrollView } from "react-native";
+import { Text, Image, View, StyleSheet, ScrollView, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import { useLayoutEffect } from "react";
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log('Pressed');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <Button title='Tap me!' onPress={headerButtonPressHandler}/>
+            }
+        });
+    }, [navigation, headerButtonPressHandler]);
+
+
     return (
         <ScrollView style={styles.rootContainer}>
             <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
